1db3bcd092514102d2ed9ebe6d2dac282496af1f
Jorgelizarraga94 <jorgelizarraga1994@gmail.com>
2025-06-14 20:45:43 -0300

    14-06-2025 at 20:45

--- src/main/java/com/example/LiterAlura/Principal/Principal.java
+++ src/main/java/com/example/LiterAlura/Principal/Principal.java
@@ -7,8 +7,6 @@ import com.example.LiterAlura.Service.ConsultaApi;
 import com.example.LiterAlura.Service.ConversorDatos;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
